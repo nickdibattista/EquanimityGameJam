fe95a1a74ee1da87ae682dd1c8c75e478b2f4958
Duncan <iron.solja.ca@gmail.com>
2023-05-26 20:18:11 -0400

    PlayerController
    +PlayerController
    +temp assets

---
+++ Equanimityv0.0.1/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera sceneCamera;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    private Vector2 mousePosition;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    //called on set amount of time physics shit
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        //Rotate player to follow mouse
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+
+    }
+}
